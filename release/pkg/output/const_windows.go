956a8b952988eec4596c1853c5761a58878daba2
imayberoot <76240748+imayberoot@users.noreply.github.com>
2022-08-30 22:20:53 -0700

    lolw
    lolw big changes

---
+++ release/pkg/output/const_windows.go
@@ -0,0 +1,13 @@
+//go:build windows
+
+package output
+
+//colors
+const (
+	TERMINAL_CLEAR_LINE = "\r\r"
+	ANSI_CLEAR          = ""
+	ANSI_RED            = ""
+	ANSI_GREEN          = ""
+	ANSI_BLUE           = ""
+	ANSI_YELLOW         = ""
+)
---
+++ release/pkg/output/output.go
@@ -0,0 +1,12 @@
+package output
+
+package output
+
+import (
+	"github.com/imayberoot/goshare/pkg/goshare"
+)
+
+func NewOutputProviderByName(name string, conf *goshare.Config) goshare.OutputProvider {
+	//We have only one outputprovider at the moment
+	return NewStdoutput(conf)
+}
